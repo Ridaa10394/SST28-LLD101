ac627fdcd900fe798564acbe9b3d40856c5da387
Ridaa <ridaa.24bcs10394@sst.scaler.com>
2026-02-22 14:19:30 +0530

    fix: question 3 using OCP principles

--- SOLID/Ex3/src/Main.java
+++ SOLID/Ex3/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
-        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
+        EligibilityEngine engine = new EligibilityEngine(
+                List.of(
+                        new DisciplinaryFlagRule(),
+                        new CgrRule(),
+                        new AttendanceRule(),
+                        new CreditsRule()
+                ),
+                new FakeEligibilityStore()
+        );
         engine.runAndPrint(s);
     }
 }
